f2bbacd4f3e6474fdae424bb93559c56e4f4ec8b
Natalya Shumshurova <nistelroy1111@mail.ru>
2012-07-12 23:18:45 +0400

    Revert "novoe"
    This reverts commit 6951fd750088a8aa48e14e1d6ec9729093ab63a6.

--- cm1.c
+++ cm1.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
 int t,k,l;
 
 int gaus(double **a ,int y,int n)
@@ -29,89 +27,46 @@ int gaus(double **a ,int y,int n)
        for(i=0; i<=n ;i++)
        { 
            if (i!=k) 
-	   {
-	      a[i][l]=0;
-	   }
+           {
+              a[i][l]=0;
+           }
        }
 return 0;
 }
 int proverca(double **a ,int n,int y)//
 {  int i,j,z;
    double min;
-   t=0;
-while (t!=2){
-     t=2;
-     for( i=0; i<n ; i++ )
+do{//цикл в котором производится изменение свободных членов, приводя к стандартному виду задачи, решаемой симплекс методом, с неотрицательными правыми частями
+     t=2;//указатель ищет отрицательные правые части
+     for( i=0; i<n ; i++ )//ищем минимальный отрицательный свободный член уравнения системы ограничений
      {
-	if (a[i][y]<0)
-	{
-	   t=t-1;
-	   if (t==1) { 	min=a[i][y];}
-	       k=i;
-	}
-	if (a[i][y]<min)
-	{
-	    min=a[i][y]; k=i;
-	}
-	if (t==2) return 0;
-     }
-     z=2;
-     for (j=0 ; j<y ; j++)
-     {
-	    if (a[k][j]<0)
-	     {    z=z-1;
-	       if (t!=2)
-	       {
-		       l=j;
-		       gaus(a,y,n);
-		       j=y;
-	       }
-	     }
-      }
-      if (z==2){ printf("Net resheniia"); return 0;}
-}
-   return 0;
-}
-void ocen(double **a, int n, int y)
-{
-  int i,j,h,b,d;
-  double v[]={0,0,0,0,0,0,0};
-  for(i=0; i<n ; i++)
-  {
-       v[i]=0;
-  }
-  for (j=0; j<y; j++)
-  { 
-        b=0; h=0;
-        for (i=0; i<n; i++)
+        if (a[i][y]<0) //ищем любой отрицательный свободный член(с.ч)
         {
-             if (a[i][j]==0)
-             {
-                 h=h+1;
-             }
-             if (a[i][j]==1) 
-	     {
-		b=b+1; d=i;
-	     }
-	 }
-	 if (b==1)
-	 {
-	      if (h==n-1)
-              {
-		   v[j]=1;
-	      }
-
-	  }else v[d]=0;
+           t=t-1;//индексируем то что нам нужно устранить отриц. своб.член
+           if (t==1) { 	min=a[i][y];} 
+	       k=i;
+        }
+        if (a[i][y]<min) //сравниваем с минимумом
+        {
+            min=a[i][y]; k=i;//приравниваем к минимуму
      }
-    printf("\n");
-    for(i=0; i<6; i++){
-       if (v[i]!=0) {
-	    v[i]=a[i][y];
+     if (t==2) return 0;//если нет отриц. своб. член. то нам нужно выйти из этой функции
+     z=2;	//счетчик проверки наличия
+     for (j=0 ; j<y ; j++)//ищем отрицательный коэффициент при х в той же строкегде был найден минимальный своб.член.
+     {
+	    if (a[k][j]<0) 
+        {    z=z-1;   
+	       if (t!=2) 
+           {
+		       l=j;
+		       gaus(a,y,n);//когда найдена разрешающая строка и разрешающий столбец
+		       j=y;
+		    }
+	    }     
       }
-      printf("\n %f111  ",v[i]);
-    }
-printf("\n");
-return;
+      if (z==2){ printf("нет решения"); return 0;}
+   }while(t!=2);
+   return 0;
 }
 int ocenki(double **a, double *bb, int n, int y )
 {  double min;
@@ -141,23 +96,25 @@ int ocenki(double **a, double *bb, int n, int y )
     j=0;
 return 0;
 }
+
+
  
 int main()
 {
-  double  **a,P1[6][7]={{2.0,1,1,0,0,64,0},{1.0,3,0,1,0,72,0},{0,1,0,0,1,20,0},{-4,-6,0.0,0,0,0,0},{0,0,0,0,0,0,0},{4,6,0.0,0,0,0,0}}, bb[]={64.0,72.0,20.0} ;
-  int i,j,n=3,y=5 ,m;
+  double  **a,P1[5][7]={{2.0,1,1,0,0,64,0},{1.0,3,0,1,0,72,0},{0,1,0,0,1,20,0},{-4,-6,0.0,0,0,0,0},{0,0,0,0,0,0,0,0}}, bb[]={64.0,72.0,20.0}  ;
+  int i,j,n=3,y=5 ,v[4]={0,0,0,0},m;// n-количество строк, y-количество столбцов
   double min;
- a=(double**)malloc(6*sizeof(double*));     
- for(i=0;i<6;i++)
+ a=(double**)malloc(5*sizeof(double*));     
+ for(i=0;i<5;i++)
    { a[i]=(double*)malloc(7*sizeof(double));
      for(j=0;j<7;j++) a[i][j]=P1[i][j];
    }
-/*       for (i=0; i<=n ;i++)
+       for (i=0; i<=n ;i++) 
        {
            for (j=0 ; j< y+1 ; j++)
-	   { printf("%.0f   ",a[i][j]); }printf("\n");
-       }*/
-proverca(a,n,y);
+           { printf("%.0f   ",a[i][j]); }printf("\n");
+       }
+//proverca(a,n,y);
 t=2;
 do
 {    ocenki(a,bb,n,y);
@@ -165,7 +122,7 @@ do
      for (j=0; j<y ; j++)
      {          
 	     if (a[n][j]<0) 
-	     {
+             {
                  t=t-1;
                  if (t==1) 
                  {
@@ -177,6 +134,7 @@ do
                  }
 	      }
      }
+     printf("AAAAAA%dAAAAAA\n",l);
      j=0;
      if(t<2)
      {
@@ -196,14 +154,12 @@ do
         }
      	gaus(a,y,n);
      }
-}while (t!=2);
-/*     for (i=0; i<=n ;i++)
+     for (i=0; i<=n ;i++) 
        {
            for (j=0 ; j<=y ; j++)
-	   { printf(" %.2f     ",a[i][j]) ; }
-	     printf("\n");
-       }*/
-ocen(a,n,y);
-getch();
+           { printf("%.1f     ",a[i][j]); }printf("\n");
+       }
+}while (t!=2);
+
 return 0;
 }
